897fe66388ec5ccbc1977d0e1c1e63079b50984f
Joanna Depowska <joannaa.depowska@gmail.com>
2020-08-19 16:05:10 +0200

    cart feature finished

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieSession = require('cookie-session');
-const authRouter = require('./routes/admin/auth');
-const productRouter = require('./routes/admin/products');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+const authRouter = require("./routes/admin/auth");
+const adminProductsRouter = require("./routes/admin/products");
+const productsRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
 
 const app = express();
 
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-	cookieSession({
-		keys: [ 'dkfjeuq021nod39r' ],
-		sameSite: 'strict'
-	})
+  cookieSession({
+    keys: ["dkfjeuq021nod39r"],
+    sameSite: "strict",
+  })
 );
 
 app.use(authRouter);
-app.use(productRouter);
+app.use(adminProductsRouter);
+app.use(productsRouter);
+app.use(cartsRouter);
 
 app.listen(port, () => {
-	console.log(`listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 });
